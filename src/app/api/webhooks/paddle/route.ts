8c1639f7610a9fca4bd64d2fa39a159e422a8276
ZAK123DSFDF <zekariyasberihun281@gmail.com>
2025-12-22 01:32:47 +0300

    feat: remove unused import for organizationPaddleAccount in route.ts

--- src/app/api/webhooks/paddle/route.ts
+++ src/app/api/webhooks/paddle/route.ts
@@ -1,11 +1,7 @@
 import { NextRequest, NextResponse } from "next/server"
 import crypto from "crypto"
 import { db } from "@/db/drizzle"
-import {
-  affiliateInvoice,
-  organizationPaddleAccount,
-  subscriptionExpiration,
-} from "@/db/schema"
+import { affiliateInvoice, subscriptionExpiration } from "@/db/schema"
 import { eq } from "drizzle-orm"
 import { calculateTrialDays } from "@/util/CalculateTrialDays"
 import { convertToUSD } from "@/util/CurrencyConvert"
