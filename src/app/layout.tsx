39cd5cc54cbda858c6f131c020e08804980394b9
ZAK123DSFDF <zekariyasberihun281@gmail.com>
2026-01-22 18:39:22 +0300

    feat: added the migration file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import QueryProvider from "@/provider/Query"
 import { Toaster } from "@/components/ui/toaster"
 import React from "react"
 import { buildMetadata } from "@/util/BuildMetadata"
+import Script from "next/script"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,6 +27,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-WHQRG09NZ7"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-WHQRG09NZ7');
+          `}
+      </Script>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <QueryProvider>{children}</QueryProvider>
         <Toaster />
